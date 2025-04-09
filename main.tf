e793809041b4ae0bb60af69ab11d20c9fad5829a
Moti <motisa94@gmail.com>
2025-04-09 19:08:13 +0200

    feat: copio los archivos del profe Jorge

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_security_group" "app_sg" {
-  name        = "my-app-security-group"
+  name        = "misecgr"
   description = "Permite trafico HTTP y SSH"
 
   ingress {
@@ -58,7 +58,7 @@ resource "aws_instance" "app_server" {
 
 
   tags = {
-    Name = "AppServer2"
+    Name = "AppServer4"
   }
 }
 output "public_ip" {
